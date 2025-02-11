25683cbbc5d66f82c3d80d7c559f4c6dfd9af2db
dikanevn <dikanevn@gmail.com>
2025-02-11 14:46:01 +0300

    Updates

---
+++ _next/static/chunks/app/layout-38d0ddbadb2c7e5c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{53648:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,23669,23)),Promise.resolve().then(n.t.bind(n,68563,23))},23669:()=>{},68563:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[252,306,441,75,358],()=>s(53648)),_N_E=e.O()}]);
--- _next/static/chunks/app/layout-5c203eb36b129d8f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3648:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,3669,23)),Promise.resolve().then(n.t.bind(n,8563,23))},3669:()=>{},8563:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[697,441,75,358],()=>s(3648)),_N_E=e.O()}]);
