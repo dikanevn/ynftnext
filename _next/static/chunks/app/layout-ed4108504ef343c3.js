872097d7d8e8e73fea827601908d486b5832501e
dikanevn <dikanevn@gmail.com>
2025-02-11 14:10:03 +0300

    Updates

---
+++ _next/static/chunks/app/layout-ed4108504ef343c3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{199:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,3669,23)),Promise.resolve().then(n.t.bind(n,9324,23))},9324:()=>{},3669:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[360,441,75,358],()=>s(199)),_N_E=e.O()}]);
